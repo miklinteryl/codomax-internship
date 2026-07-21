ee9702cbc1bacb8691c7ac71fd20b7ee30ec8bf7
Miklin Teryl <miklin01072006@example.com>
2026-07-21 20:31:16 +0530

    Day 5: Created Express server with GET and POST routes

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
+// Middleware to parse data submitted from HTML forms
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// GET Route: This handles requests to the homepage
 app.get('/', (req, res) => {
-    res.send('Hello World! My first Codomax task is complete!');
+    res.send('Welcome to the Blog Backend Server! This is a GET request.');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+// GET Route: This could eventually serve your Add Blog HTML page
+app.get('/add-blog', (req, res) => {
+    res.send('This route will eventually send the add-blog.html file.');
+});
+
+// POST Route: This will receive the form data when a user hits "Publish"
+app.post('/submit-blog', (req, res) => {
+    const blogTitle = req.body.title;
+    console.log('New blog submitted with title:', blogTitle);
+    
+    // Send a response back to the browser
+    res.send('Blog post successfully received by the server! (POST request successful)');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Backend server is running on http://localhost:${port}`);
 });
